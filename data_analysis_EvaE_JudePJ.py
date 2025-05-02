2a4a139022dcb0f5e283f17343dee7a8f92a9566
eva-elef <eva.a.eleftheriou@gmail.com>
2025-05-02 17:23:51 -0400

    Update data_analysis_EvaE_JudePJ.py
    started cleaning up latest and first

--- data_analysis_EvaE_JudePJ.py
+++ data_analysis_EvaE_JudePJ.py
@@ -22,41 +22,28 @@ with open('Dogs Issued Dangerous Dog Orders.csv', newline='') as file: # Reads c
         dog = Dog(row)
         dogs.append(dog)
 
-def max_min_date(l,string):
-    print(f"The maximum {string} where an attack occurred is {max(l)}, and the minimum is {min(l)}")
+def max_min_date(when,string):
+    print(f"The maximum {string} where an attack occurred is {max(when)}, and the minimum is {min(when)}")
 
 max_min_date([dog.date.day for dog in dogs], "day")
-# list_dogs_year = [dog.date.year for dog in dogs]
-# max_year = max(list_dogs_year)
-# min_year = min(list_dogs_year)
-# print(f"The most recent (maximum) year there was a dog attack was: {max_year}")
-# print(f"The first (minimum) year there was a dog attack was: {min_year}")
-# 
-# list_dogs_month = [dog.date.month for dog in dogs]
-# max_month = (max(list_dogs_month))
-# min_month = (min(list_dogs_month))
-# print(f"The most recent (maximum) month there was a dog attack was: {max_month}")
-# print(f"The first (minimum) month there was a dog attack was: {min_month}")
-# 
-# list_dogs_day = [dog.date.day for dog in dogs]
-# max_day = (max(list_dogs_day))
-# min_day = (min(list_dogs_day))
-# print(f"The most recent (maximum) day there was a dog attack was: {max_day}")
-# print(f"The first (minimum) day there was a dog attack was: {min_day}")
+max_min_date([dog.date.month for dog in dogs], "month")
+max_min_date([dog.date.year for dog in dogs], "year")
 
-#list_dogs_dates = [dog.date.day for dog in dogs if dog.date.year == 2017 and dog.date.month ==2]
-#print(min(list_dogs_dates))
-# latest_dogs_month = [dog.date.month for dog in dogs if dog.date.year == max_year]
-# latest_month = max(latest_dogs_month)
-# latest_dogs_day = [dog.date.day for dog in dogs if dog.date.year == max_year and dog.date.month == latest_month]
-# print(f"The most recent dog attack took place on {max_year} / {latest_month} / {max(latest_dogs_day)}")
-# 
-# first_dogs_month = [dog.date.month for dog in dogs if dog.date.year == min_year]
-# first_month = min(first_dogs_month)
-# first_dogs_day = [dog.date.day for dog in dogs if dog.date.year == min_year and dog.date.month == first_month]
-# print(f"The first ever dog attack took place on {min_year} / {first_month} / {min(first_dogs_day)}")
-# 
-# 
+
+def latest_date(year,month,day):
+        print(f"The latest day where a dog attack occurred was on {max(year)}/{max(month)}/{max(day)}")
+
+latest_date([dog.date.year for dog in dogs], [dog.date.month for dog in dogs if dog.date.year == 2025],
+            [dog.date.day for dog in dogs if dog.date.month==3 and dog.date.year == 2025])
+
+def first_date(year,month,day):
+        print(f"The first day where a dog attack occurred was on {min(year)}/{min(month)}/{min(day)}")
+
+first_date([dog.date.year for dog in dogs], [dog.date.month for dog in dogs if dog.date.year == 2017],
+            [dog.date.day for dog in dogs if min(dog.date.month) and dog.date.year = 2017])
+
+
+
 '''
 EXAMPLES
 '''
