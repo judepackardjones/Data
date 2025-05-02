531f1101a459a0b31f30cc06f5831f8098bab8f5
Jude Packard-Jones <judepackardjones@gmail.com>
2025-05-02 10:44:11 -0400

    Added file reading and dog object

--- data_analysis_EvaE_JudePJ.py
+++ data_analysis_EvaE_JudePJ.py
@@ -1,3 +1,24 @@
-print("Hello world")
-print("test")
-# test test 
+from csv import reader
+
+dogs = []
+class Dog:
+    def __init__(self, name, colours, ward_name, bite, date):
+        self.name = name
+        self.colours = colours
+        self.ward_name = ward_name
+        self.bite = bite
+        self.date = date
+
+class Date:
+    def __init__(self, date_string):
+        l = date_string.split("-")
+        self.day = l[2]
+        self.month = l[1]
+        self.year = l[0]
+
+
+with open('Dogs Issued Dangerous Dog Orders.csv', newline='') as file:
+    read = reader(file)
+    for row in read:
+        dog = Dog(row[2], row[4], row[6], row[7], Date(row[9]))
+        dogs.append(dog)
